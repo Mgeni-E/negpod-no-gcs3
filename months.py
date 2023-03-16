1077f9955c4b184876ca61c8cfe73ef315b10672
Mgeni-E <e.mgeni@alustudent>
2023-03-16 11:28:31 +0200

    gdgdgdgs

---
+++ months.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+month =input("Enter the month: ")
+if month in ["January", "March", "May", "July", "August", "October", "December"]:
+    print("Month has 31 days")
+elif month in ["April", "June", "September", "November"]:
+    print("Month has 30 days")
+elif month == "February":
+    print("month has 28 or 29 days")
+else:
+    print("Month input us invalid")
+    
+
+
+
